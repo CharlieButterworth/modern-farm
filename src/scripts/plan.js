b01db5a1b7d730b6b6fae68ab984231315d8a825
Charles Butterworth <charlessbutterworth@gmail.com>
2020-10-08 15:52:43 -0500

    committing to save all work from today

--- src/scripts/plan.js
+++ src/scripts/plan.js
@@ -1,10 +1,10 @@
 const types = ["Soybean", "Corn", "Asparagus", "Wheat", "Potato", "Sunflower"]
 
 export const createPlan = () => {
-    const plan = []
+    const plan = [types]
 
     for (let index = 0; index < 3; index++) {
-        const row = []
+        const row = [types]
         for (let j = 0; j < 6; j++) {
             row.push(crop.next().value)
         }
@@ -16,17 +16,16 @@ export const createPlan = () => {
 
 
 
-
 /*
     This is a generator function used to randomly
     generate crop types to plants in each plot. It
     is not relevant for you implementing your code
     for this project.
 */
-const crop = function* () {
-    while (true) {
-        const typeIdx = Math.floor(Math.random() * types.length)
-        yield types[typeIdx]
-    }
-}()
+// const crop = function* () {
+//     while (true) {
+//         const typeIdx = Math.floor(Math.random() * types.length)
+//         yield types[typeIdx]
+//     }
+// }()
 
